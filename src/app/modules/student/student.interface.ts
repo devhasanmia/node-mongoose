aac734bfbf650681fdc892b72b26257c89f1406b
devhasanmia <dev.hasanmia@gmail.com>
2024-05-17 15:54:41 +0600

    first commit

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,39 @@
+import {} from 'mongoose';
+
+export type Guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNumber: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNumber: string;
+};
+export type BloodGroup =
+  | 'A+'
+  | 'A-'
+  | 'B+'
+  | 'B-'
+  | 'AB+'
+  | 'AB-'
+  | 'O+'
+  | 'O-';
+export type Name = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+
+
+export type Student = {
+  name: Name;
+  gender: 'male' | 'female';
+  email: string;
+  dateOfBirth: string;
+  contactNumber: string;
+  emargencyContactNumber: string;
+  BloodGroup?: BloodGroup;
+  avatar?: string;
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+};
---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import app from './app';
+import config from './app/config';
+import mongoose from 'mongoose';
+
+async function main() {
+  try {
+    await mongoose.connect(config.databaseURL as string, );
+    app.listen(config.port, () => {
+      console.log(`Server is running on port ${config.port}`);
+    });
+  } catch (error) {
+    console.error('Failed to start server:', error);
+  }
+}
+
+main().catch((err) => console.error('Unexpected error in main():', err));
