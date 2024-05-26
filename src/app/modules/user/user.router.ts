bcd8f47b2042bf972721da02d0651029db14fb51
devhasanmia <dev.hasanmia@gmail.com>
2024-05-26 12:19:01 +0600

    Refactor zod validation

--- src/app/modules/user/user.router.ts
+++ src/app/modules/user/user.router.ts
@@ -1,13 +1,27 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { UserController } from './user.controller';
+import { AnyZodObject } from 'zod';
+import { studentValidationSchema } from '../student/student.validation';
 const router = express.Router();
 
-const shenaBahini = (req: Request, res: Response, next: NextFunction) => {
-  console.log('Thamo Tomar tikit Daw');
-  console.log(req.body);
-  next()
+const validateRequest = (schema: AnyZodObject) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    console.log(schema);
+    try {
+      await schema.parseAsync({
+        body: req.body,
+      });
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
 };
 
-router.post('/create-user', shenaBahini, UserController.createStudent);
+router.post(
+  '/create-user',
+  validateRequest(studentValidationSchema),
+  UserController.createStudent,
+);
 
 export const UserRouter = router;
